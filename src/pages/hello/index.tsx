d281e5874157b858daa9f0d6f2585e697b441234
lxh <yangguo520@>
2023-07-06 10:30:34 +0800

    feat: 🚀添加eslint + husky + prettier

--- src/pages/hello/index.tsx
+++ src/pages/hello/index.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
-    return (
-        <>
-            123
-        </>
-    )
+  return (
+    <>
+      123
+    </>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,12 @@ export default function Home() {
     <>
       <Head>
         <title>Acfun</title>
-        <meta name="description" content={"你就猜猜是干啥的吧"}/>
+        <meta name="description" content="你就猜猜是干啥的吧"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <main className={`${styles.main}`}>
-        <button onClick={goHello}>hello</button>
+        <button title="111" type="button" onClick={goHello}>hello</button>
       </main>
     </>
   )
